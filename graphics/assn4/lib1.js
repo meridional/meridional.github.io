c38b8e074d97e916ffa13c8085cd609de54e60e6
Ye Ji <yeji1989@me.com>
2015-03-03 21:28:33 -0500

    first publishable

--- graphics/assn4/lib1.js
+++ graphics/assn4/lib1.js
@@ -12,6 +12,11 @@ Vector3.prototype = {
       this.y = y;
       this.z = z;
    },
+   copy: function(v) {
+      this.x = v.x;
+      this.y = v.y;
+      this.z = v.z;
+   }
 }
 
 function Vector4() {
@@ -51,9 +56,100 @@ function start_gl(canvas_id, vertexShader, fragmentShader) {
       // Initialize gl. Then start the frame loop.
 
       gl_init(gl, vertexShader, fragmentShader);
+      initCube(gl);
       gl_update(gl);
 
    }, 100); // Wait 100 milliseconds before starting gl.
+}
+
+function initCube(gl) {
+      gl.cubeAnchor = new Vector3();
+      gl.cubeAnchor.set(-2,2,-10);
+      gl.cubeSideLength = 4.0;
+      gl.cubeDir1 = new Vector3()
+      gl.cubeDir1.set(1,0,0);
+      gl.cubeDir2 = new Vector3()
+      gl.cubeDir2.set(0,1,0);
+}
+
+function dotProduct(v1, v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+function crossProduct(v1,v2) {
+   var r = new Vector3();
+   r.set(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+   return r;
+}
+
+function vNormalize(v) {
+   var r = new Vector3();
+   var s = dotProduct(v,v);
+   r.x = v.x / s;
+   r.y = v.y / s;
+   r.z = v.z / s;
+   return r;
+}
+
+function gl_update_uCube(gl) {
+  var alpha = gl.time;
+  var beta = gl.time * 2;
+  gl.cubeDir1.set(Math.sin(alpha), Math.cos(alpha) * Math.sin(beta), Math.cos(alpha) * Math.cos(beta));
+  gl.cubeDir2.set(Math.cos(alpha), -Math.sin(alpha) * Math.sin(beta), -Math.sin(alpha) * Math.cos(beta));
+  gl.cubeDir3 = crossProduct(gl.cubeDir1, gl.cubeDir2);
+  var cubeDir1 = gl.cubeDir1;
+  var cubeDir2 = gl.cubeDir2;
+  var cubeDir3 = new Vector3();
+  var dir = 1.0;
+  var walls = new Float32Array(24);
+  var atmp = new Vector3()
+  gl.cubeAnchor.x = Math.sin(gl.time);
+  gl.cubeAnchor.y = Math.cos(gl.time);
+  atmp.copy(gl.cubeAnchor);
+  var cubeSideLength = gl.cubeSideLength;
+  var normals = new Float32Array(3*6);
+  var mats = new Float32Array(3*6);
+  var spces = new Float32Array(24);
+  for (i = 0; i < 6; i++) {
+      cubeDir3 = vNormalize(crossProduct(cubeDir1, cubeDir2));
+      walls[4*i] = -cubeDir3.x;
+      walls[4*i+1] = -cubeDir3.y;
+      walls[4*i+2] = -cubeDir3.z;
+      walls[4*i+3] = dotProduct(cubeDir3, atmp);
+      normals[3*i] = -walls[4*i];
+      normals[3*i+1] = -walls[4*i+1];
+      normals[3*i+2] = -walls[4*i+2];
+      mats[3*i] = .5;
+      mats[3*i+1] = .5;
+      mats[3*i+2] = .5;
+      spces[4*i] = .9;
+      spces[4*i+1] = .9;
+      spces[4*i+2] = .9;
+      spces[4*i+3] = 100;
+
+      //wallMats[i] = vec3(.8,.8,.8);
+      //wallSpecs[i] = vec4(1.,1.,1.,10.);
+      atmp.x = atmp.x + cubeDir1.x * cubeSideLength;
+      atmp.z = atmp.z + cubeDir1.z * cubeSideLength;
+      atmp.y = atmp.y + cubeDir1.y * cubeSideLength;
+      cubeDir1.x = dir * cubeDir2.x;
+      cubeDir1.y = dir * cubeDir2.y;
+      cubeDir1.z = dir * cubeDir2.z;
+      cubeDir2.x = dir * cubeDir3.x;
+      cubeDir2.y = dir * cubeDir3.y;
+      cubeDir2.z = dir * cubeDir3.z;
+      dir *= -1.0;
+  }
+  //console.log(normals);
+  gl.uniform4fv(gl.uWalls, walls);
+  gl.uniform3fv(gl.uwNormals, normals);
+  gl.uniform4fv(gl.uCubeSpecs, spces);
+  gl.uniform3fv(gl.uCubeMats, mats);
+  gl.uniform1f(gl.uCubeSideLength, cubeSideLength);
+  gl.uniform3f(gl.uCubeDir1, gl.cubeDir1.x, gl.cubeDir1.y, gl.cubeDir1.z);
+  gl.uniform3f(gl.uCubeDir2, gl.cubeDir2.x, gl.cubeDir2.y, gl.cubeDir2.z);
+  gl.uniform3f(gl.uCubeDir3, gl.cubeDir3.x, gl.cubeDir3.y, gl.cubeDir3.z);
+  gl.uniform3f(gl.uCubeAnchor, gl.cubeAnchor.x, gl.cubeAnchor.y, gl.cubeAnchor.z);
 }
 
 // Function to create and attach a shader to a gl program.
@@ -95,20 +191,30 @@ function gl_init(gl, vertexShader, fragmentShader) {
 
    // Get the address in the fragment shader of each of my uniform variables.
 
-   ['uTime','uCursor'].forEach(function(name) {
+   ['uTime','uCursor','uWalls','uCubeSideLength','uCubeMats','uCubeSpecs','uwNormals','uCubeAnchor','uCubeDir1','uCubeDir2','uCubeDir3', 'uSphere', 'uRefraction'].forEach(function(name) {
       gl[name] = gl.getUniformLocation(program, name);
    });
+
 }
 
 // Update is called once per animation frame.
 
+
 function gl_update(gl) {
    var time = ((new Date()).getTime() - startTime) / 1000;            // Set uniform variables
+   gl.time = time;
    gl.uniform1f(gl.uTime  , time);                                    // in fragment shader.
+   gl.uniform1f(gl.uRefraction, 1.5);
    gl.uniform3f(gl.uCursor, gl.cursor.x, gl.cursor.y, gl.cursor.z);
+   gl_update_uCube(gl);
    //gl.uniform1i(gl.uCount, 2);
+   gl_update_sphere(gl);
    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);                            // Render the square.
    requestAnimFrame(function() { gl_update(gl); });                   // Animate.
+}
+
+function gl_update_sphere(gl) {
+   gl.uniform4fv(gl.uSphere, new Float32Array([gl.cursor.x*5,gl.cursor.y*5,-4.,1.,1.,1.,1.,1.]));
 }
 
 // A browser-independent way to call a function after 1/60 second.
