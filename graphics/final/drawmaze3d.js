efd076214328ef81c8e6b5fb3b77c2c13be47be3
Ye Ji <yeji1989@me.com>
2015-05-16 19:18:16 -0400

    first working draft

--- graphics/final/drawmaze3d.js
+++ graphics/final/drawmaze3d.js
@@ -5,14 +5,53 @@ var wallLength = 2;
 var wallHeight = 2;
 var turnspeed = .05;
 var speed = .05;
-var ThreeJSTest = (function () {
-    function ThreeJSTest(rows, cols) {
+var MazeScene = (function () {
+    function MazeScene() {
+        this.scene = new THREE.Scene();
+    }
+    MazeScene.prototype.tryJump = function () {
+    };
+    MazeScene.prototype.currentDirection = function () {
+        return new THREE.Vector3(Math.sin(this.direction), -0., Math.cos(this.direction));
+    };
+    MazeScene.prototype.changeDirection = function (delta) {
+        this.direction += delta;
+        this.camera.rotateY(delta);
+    };
+    MazeScene.prototype.turnLeft = function () {
+        this.changeDirection(turnspeed);
+    };
+    MazeScene.prototype.turnRight = function () {
+        this.changeDirection(-turnspeed);
+    };
+    MazeScene.prototype.collisiontest = function (forward) {
+        var dir = this.currentDirection().multiplyScalar(forward ? 1 : -1);
+        var tracer = new THREE.Raycaster(new THREE.Vector3(this.camera.position.x, 0, this.camera.position.z), dir, 0, 0.4);
+        var int = tracer.intersectObject(this.walls);
+        return int.length > 0;
+    };
+    MazeScene.prototype.moveForward = function () {
+        if (this.collisiontest(true))
+            return;
+        this.camera.position.add(this.currentDirection().multiplyScalar(speed));
+    };
+    MazeScene.prototype.moveBackward = function () {
+        if (this.collisiontest(false))
+            return;
+        this.camera.position.add(this.currentDirection().multiplyScalar(-speed));
+    };
+    MazeScene.prototype.render = function (renderer) {
+        renderer.render(this.scene, this.camera);
+    };
+    MazeScene.prototype.set = function (rows, cols) {
+        for (var i = this.scene.children.length - 1; i >= 0; i--) {
+            this.scene.remove(this.scene.children[i]);
+        }
         //return;
         function indexToCoordinate(i, j) {
-            return new THREE.Vector3(i - (rows / 2), 0, j - cols / 2).multiplyScalar(wallLength);
+            return new THREE.Vector3(i - rows / 2, 0, j - cols / 2).multiplyScalar(wallLength);
         }
         this.edges = kruskal(rows, cols);
-        console.log(this.edges.length);
         this.rows = rows;
         this.cols = cols;
         var horizontalBox = new THREE.BoxGeometry(wallLength, wallHeight, wallWidth);
@@ -23,11 +62,6 @@ var ThreeJSTest = (function () {
         //texture.wrapT = THREE.RepeatWrapping;
         var mat = new THREE.MeshPhongMaterial({ map: texture, wireframe: false });
         //var mat = new THREE.MeshNormalMaterial();
-        this.renderer = new THREE.WebGLRenderer({ alpha: true });
-        this.renderer.setSize(800, 800);
-        this.renderer.setClearColor(0xFFFFFF, 1);
-        // Bind the renderer to the HTML, parenting it to our 'content' DIV
-        document.getElementById('content').appendChild(this.renderer.domElement);
         // Create a Scene
         var scene = new THREE.Scene();
         this.scene = scene;
@@ -56,80 +90,86 @@ var ThreeJSTest = (function () {
         }
         this.walls = new THREE.Mesh(wallsGeo, mat);
         scene.add(this.walls);
-        this.scene.add(new THREE.AmbientLight(0xffffff));
+        this.scene.add(new THREE.AmbientLight(0x0f0f0f));
         var floortext = THREE.ImageUtils.loadTexture("floor.jpg");
         var plane = new THREE.PlaneBufferGeometry(cols * wallLength * 2, rows * wallLength * 2);
-        var floor = new THREE.Mesh(plane, new THREE.MeshPhongMaterial({ color: 0xaaaaaa, wireframe: false, map: floortext, side: THREE.DoubleSide, repeat: [1, 1] }));
+        var floor = new THREE.Mesh(plane, new THREE.MeshPhongMaterial({ color: 0xaaaaaa, wireframe: false, map: floortext, side: THREE.DoubleSide, repeat: [rows / 2, cols / 2] }));
         floor.rotateX(Math.PI / 2);
         floor.receiveShadow = true;
+        var ceil = new THREE.Mesh(plane, new THREE.MeshPhongMaterial({
+            map: floortext,
+            side: THREE.DoubleSide,
+            repeat: [rows, cols]
+        }));
+        ceil.rotateX(Math.PI / 2);
+        ceil.position.set(0, wallHeight / 2, 0);
+        scene.add(ceil);
         scene.add(floor);
-        this.camera = new THREE.PerspectiveCamera(50, 1, .1, 2000);
-        this.camera.position.set(0, 2.5, 0);
-        this.camera.lookAt(new THREE.Vector3(0., 2.5, 1.));
-        this.direction = 0;
+        this.camera = new THREE.PerspectiveCamera(50, 800 / 600, .1, 2000);
+        var cameraHeight = 0.5;
+        var beginning = indexToCoordinate(0, 0);
+        var spotlight = new THREE.PointLight(0xffffff, 1, 10);
+        this.camera.add(spotlight);
+        //spotlight.lookAt(new THREE.Vector3(0., cameraHeight,1.));
         this.scene.add(this.camera);
-        this.renderer.render(this.scene, this.camera);
-    }
-    ThreeJSTest.prototype.currentDirection = function () {
-        return new THREE.Vector3(Math.sin(this.direction), -0., Math.cos(this.direction));
+        this.camera.position.set(beginning.x, cameraHeight, beginning.z);
+        this.camera.lookAt(new THREE.Vector3(0., cameraHeight, 0));
+        var xx = new THREE.Vector3().subVectors(new THREE.Vector3(0, cameraHeight, 0), this.camera.position);
+        this.direction = Math.atan2(xx.x, xx.z);
+        this.theEnd = new THREE.Mesh(new THREE.SphereGeometry(.2), new THREE.MeshNormalMaterial());
+        this.scene.add(this.theEnd);
+        var endCoord = indexToCoordinate(rows - 1, cols - 1);
+        this.theEnd.position.set(endCoord.x, endCoord.y, endCoord.z);
+        this.theEnd.position.setY(cameraHeight);
+        this.theEnd.position.y = cameraHeight;
     };
-    ThreeJSTest.prototype.changeDirection = function (delta) {
-        this.direction += delta;
-        this.camera.rotateY(delta);
+    MazeScene.prototype.goalReached = function () {
+        return this.camera.position.distanceTo(this.theEnd.position) < wallLength / 5;
     };
-    ThreeJSTest.prototype.turnLeft = function () {
-        this.changeDirection(turnspeed);
-    };
-    ThreeJSTest.prototype.turnRight = function () {
-        this.changeDirection(-turnspeed);
-    };
-    ThreeJSTest.prototype.collisiontest = function (forward) {
-        var dir = this.currentDirection().multiplyScalar(forward ? 1 : -1);
-        var tracer = new THREE.Raycaster(new THREE.Vector3(this.camera.position.x, 0, this.camera.position.z), dir, 0, 0.4);
-        var int = tracer.intersectObject(this.walls);
-        return int.length > 0;
-    };
-    ThreeJSTest.prototype.moveForward = function () {
-        if (this.collisiontest(true))
-            return;
-        this.camera.position.add(this.currentDirection().multiplyScalar(speed));
-    };
-    ThreeJSTest.prototype.moveBackward = function () {
-        if (this.collisiontest(false))
-            return;
-        this.camera.position.add(this.currentDirection().multiplyScalar(-speed));
-    };
-    ThreeJSTest.prototype.render = function () {
-        this.renderer.render(this.scene, this.camera);
-    };
-    ThreeJSTest.prototype.turn = function () {
-        //this.lightTarget.rotateY(.01);
-    };
-    return ThreeJSTest;
+    return MazeScene;
 })();
-function start(test) {
-    test.turn();
-    test.renderer.render(test.scene, test.camera);
+function hijackKD(mazeScene) {
+    kd.W.down(function () {
+        mazeScene.moveForward();
+    });
+    kd.A.down(function () {
+        mazeScene.turnLeft();
+    });
+    kd.D.down(function () {
+        mazeScene.turnRight();
+    });
+    kd.S.down(function () {
+        mazeScene.moveBackward();
+    });
+}
+function unbind() {
+    kd.W.unbindDown();
+    kd.A.unbindDown();
+    kd.D.unbindDown();
+    kd.S.unbindDown();
+}
+function start(mazeScene, renderer) {
     kd.tick();
+    if (mazeScene.goalReached()) {
+        //alert("next level will be " + mazeScene.rows * 2 + " x " + mazeScene.cols * 2);
+        mazeScene.set(mazeScene.rows * 2, mazeScene.cols * 2); // = new MazeScene(mazeScene.rows * 2, mazeScene.cols * 2);
+    }
+    mazeScene.render(renderer);
     requestAnimationFrame(function () {
-        start(test);
+        start(mazeScene, renderer);
     });
 }
 window.onload = function () {
     console.log("hello");
-    var three = new ThreeJSTest(20, 20);
-    kd.W.down(function () {
-        three.moveForward();
-    });
-    kd.A.down(function () {
-        three.turnLeft();
-    });
-    kd.D.down(function () {
-        three.turnRight();
-    });
-    kd.S.down(function () {
-        three.moveBackward();
-    });
-    start(three);
+    var three = new MazeScene();
+    three.set(2, 2);
+    hijackKD(three);
+    //kd.SpaceKey.down(function() {three.tryJump();});
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(800, 600);
+    renderer.setClearColor(0x000000, 1);
+    // Bind the renderer to the HTML, parenting it to our 'content' DIV
+    document.getElementById('content').appendChild(renderer.domElement);
+    start(three, renderer);
 };
 //# sourceMappingURL=drawmaze3d.js.map
--- graphics/final/drawmaze3d.ts
+++ graphics/final/drawmaze3d.ts
@@ -7,8 +7,7 @@ var wallHeight = 2;
 var turnspeed = .05;
 var speed = .05;
 
-class ThreeJSTest {
-  renderer: THREE.WebGLRenderer;
+class MazeScene {
   scene: THREE.Scene;
   camera: THREE.Camera;
   walls : THREE.Mesh;
@@ -17,6 +16,11 @@ class ThreeJSTest {
   cols : number;
   light : THREE.Light;
   direction : number;
+  theEnd : THREE.Mesh;
+
+  tryJump() : void {
+
+  }
 
   currentDirection() : THREE.Vector3 {
     return new THREE.Vector3(Math.sin(this.direction), -0., Math.cos(this.direction));
@@ -52,17 +56,24 @@ class ThreeJSTest {
     this.camera.position.add(this.currentDirection().multiplyScalar(-speed));
   }
 
-  render() {
-    this.renderer.render(this.scene, this.camera);
+  render(renderer : THREE.WebGLRenderer)  : void {
+    renderer.render(this.scene, this.camera);
   }
 
-  constructor(rows : number, cols : number) {
+  constructor() {
+    this.scene = new THREE.Scene();
+  }
+
+
+  set(rows : number, cols : number) {
+    for (var i = this.scene.children.length - 1; i >= 0; i--) {
+      this.scene.remove(this.scene.children[i]);
+    }
     //return;
-    function indexToCoordinate(i, j) : THREE.Vector3 {
-      return new THREE.Vector3(i - (rows / 2), 0, j - cols / 2).multiplyScalar(wallLength);
+    function indexToCoordinate(i : number, j : number) : THREE.Vector3 {
+      return new THREE.Vector3(i - rows / 2, 0, j - cols / 2).multiplyScalar(wallLength);
     }
     this.edges = kruskal(rows, cols);
-    console.log(this.edges.length);
     this.rows = rows;
     this.cols = cols;
     var horizontalBox = new THREE.BoxGeometry(wallLength, wallHeight, wallWidth);
@@ -76,12 +87,6 @@ class ThreeJSTest {
     //var mat = new THREE.MeshNormalMaterial();
 
 
-    this.renderer = new THREE.WebGLRenderer({ alpha: true });
-    this.renderer.setSize(800,800);
-    this.renderer.setClearColor(0xFFFFFF,1);
-
-    // Bind the renderer to the HTML, parenting it to our 'content' DIV
-    document.getElementById('content').appendChild(this.renderer.domElement);
 
     // Create a Scene
     var scene = new THREE.Scene();
@@ -112,7 +117,7 @@ class ThreeJSTest {
     }
     this.walls = new THREE.Mesh(wallsGeo, mat);
     scene.add(this.walls);
-    this.scene.add(new THREE.AmbientLight(0xffffff));
+    this.scene.add(new THREE.AmbientLight(0x0f0f0f));
     var floortext = THREE.ImageUtils.loadTexture("floor.jpg");
 
     var plane = new THREE.PlaneBufferGeometry(cols * wallLength * 2, rows * wallLength * 2);
@@ -122,44 +127,91 @@ class ThreeJSTest {
           wireframe:false,
           map:floortext,
           side:THREE.DoubleSide,
-        repeat:[1,1]}));
-
+        repeat:[rows/2,cols/2]}));
     floor.rotateX(Math.PI / 2);
     floor.receiveShadow = true;
+    var ceil = new THREE.Mesh(plane,
+      new THREE.MeshPhongMaterial({
+          map:floortext
+        , side:THREE.DoubleSide
+        , repeat:[rows, cols]}));
+    ceil.rotateX(Math.PI/2);
+    ceil.position.set(0, wallHeight/2, 0);
+    scene.add(ceil);
     scene.add(floor);
     this.camera = new THREE.PerspectiveCamera(50
-        , 1
+        , 800/600
         , .1, 2000);
 
 
-    this.camera.position.set(0,2.5,0);
-    this.camera.lookAt(new THREE.Vector3(0.,2.5,1.));
-    this.direction = 0;
-
+    var cameraHeight = 0.5;
+    var beginning = indexToCoordinate(0, 0);
+    var spotlight = new THREE.PointLight(0xffffff,1,10);
+    this.camera.add(spotlight);
+    //spotlight.lookAt(new THREE.Vector3(0., cameraHeight,1.));
     this.scene.add(this.camera);
+    this.camera.position.set(beginning.x, cameraHeight, beginning.z);
+    this.camera.lookAt(new THREE.Vector3(0., cameraHeight,0));
+    var xx = new THREE.Vector3().subVectors(new THREE.Vector3(0,cameraHeight,0), this.camera.position);
+    this.direction = Math.atan2(xx.x, xx.z);
+    this.theEnd = new THREE.Mesh(new THREE.SphereGeometry(.2), new THREE.MeshNormalMaterial());
+    this.scene.add(this.theEnd);
+    var endCoord = indexToCoordinate(rows-1, cols-1);
+    this.theEnd.position.set(endCoord.x, endCoord.y, endCoord.z);
+    this.theEnd.position.setY(cameraHeight);
 
-    this.renderer.render(this.scene,this.camera);
+    this.theEnd.position.y = cameraHeight;
   }
 
-  turn() {
-    //this.lightTarget.rotateY(.01);
+
+
+  goalReached() : boolean {
+    return this.camera.position.distanceTo(this.theEnd.position) < wallLength/5;
   }
 }
 
 
-function start(test : ThreeJSTest) {
-  test.turn();
-  test.renderer.render(test.scene, test.camera);
+function hijackKD(mazeScene : MazeScene) : void {
+
+  kd.W.down(function() {mazeScene.moveForward();});
+  kd.A.down(function() {mazeScene.turnLeft();});
+  kd.D.down(function() {mazeScene.turnRight();});
+  kd.S.down(function() {mazeScene.moveBackward();});
+}
+
+function unbind() {
+  kd.W.unbindDown();
+  kd.A.unbindDown();
+  kd.D.unbindDown();
+  kd.S.unbindDown();
+}
+
+
+function start(mazeScene : MazeScene, renderer : THREE.WebGLRenderer) {
   kd.tick();
-  requestAnimationFrame(() => {start(test)});
+  if (mazeScene.goalReached()) {
+
+
+    //alert("next level will be " + mazeScene.rows * 2 + " x " + mazeScene.cols * 2);
+    mazeScene.set(mazeScene.rows * 2, mazeScene.cols * 2);// = new MazeScene(mazeScene.rows * 2, mazeScene.cols * 2);
+    //hijackKD(mazeScene);
+
+  }
+  mazeScene.render(renderer);
+  requestAnimationFrame(() => {start(mazeScene, renderer)});
 }
 
 window.onload = () => {
   console.log("hello");
-  var three = new ThreeJSTest(20, 20);
-  kd.W.down(function() {three.moveForward();});
-  kd.A.down(function() {three.turnLeft();});
-  kd.D.down(function() {three.turnRight();});
-  kd.S.down(function() {three.moveBackward();});
-  start(three);
+  var three = new MazeScene();
+  three.set(2,2);
+  hijackKD(three);
+  //kd.SpaceKey.down(function() {three.tryJump();});
+  var renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(800,600);
+  renderer.setClearColor(0x000000,1);
+
+  // Bind the renderer to the HTML, parenting it to our 'content' DIV
+  document.getElementById('content').appendChild(renderer.domElement);
+  start(three, renderer);
 };
